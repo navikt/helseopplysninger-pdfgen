ccbed16ef1491855c51fe04461195046069bcbd2
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2023-12-04 12:25:07 +0100

    oppgradere auto script med nyeste versjon av pdfgen

--- run_development.sh
+++ run_development.sh
@@ -2,7 +2,7 @@
 
 CURRENT_PATH="$(cd "$(dirname "$1")" || exit; pwd)/$(basename "$1")"
 
-docker pull ghcr.io/navikt/pdfgen:2.0.17
+docker pull ghcr.io/navikt/pdfgen:2.0.19
 docker run \
         -v "$CURRENT_PATH"/templates:/app/templates \
         -v "$CURRENT_PATH"/fonts:/app/fonts \
